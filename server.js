3f3d90872666e602cb9ac83e665d39f3c4f95b66
rk-codes <ping.you.back@gmail.com>
2018-06-19 12:46:08 -0700

    add user registration

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyparser = require('body-parser');
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
 
 const app = express();
-const port = process.env.PORT || 5000;
+
+//Body parser middleware
+app.use(bodyparser.json());
 
 //DB config
 const db = require('./config/keys').mongoURI;
@@ -21,6 +24,8 @@ app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 
+
 app.get('/', (req, res) => res.send('Hello'));
 
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
