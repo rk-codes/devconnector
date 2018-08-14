aec48123a2e8e087c4ab29effec74126d55f6ead
rk-codes <ping.you.back@gmail.com>
2018-08-14 13:52:17 -0700

    add endpoints for profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -19,6 +19,7 @@ router.get('/test', (req, res) => res.json({msg: "Profile Works"}));
 router.get('/', passport.authenticate('jwt', {session: false}), (req, res) => {
     const errors = {};
     Profile.findOne({user: req.user.id})
+    .populate('user', ['name', 'avatar'])
     .then(profile => {
         if(!profile) {
             errors.noProfile = 'There is no profile for the user';
@@ -28,6 +29,51 @@ router.get('/', passport.authenticate('jwt', {session: false}), (req, res) => {
     })
     .catch(err => res.status(404).json(err));
 });
+
+// GET api/profile/user/:userId
+// Get profile by user id
+router.get('/user/:userId', (req, res) => {
+    const errors = {};
+    Profile.findOne({user: req.params.userId})
+    .populate('user', ['name', 'avatar'])
+    .then(profile => {
+        if(!profile) {
+            errors.noProfile = 'There is no profile for this user';
+            return res.status(404).json(errors);
+        }
+    })
+    .catch(err => res.status(404).json({profile: 'There is no profile for this user'}));
+})
+
+// GET api/profile/handle/:handle
+// Get profile by handle
+router.get('/handle/:handle', (req, res) => {
+    const errors = {};
+    Profile.findOne({user: req.params.handle})
+    .populate('user', ['name', 'avatar'])
+    .then(profile => {
+        if(!profile) {
+            errors.noProfile = 'There is no profile for this user';
+            return res.status(404).json(errors);
+        }
+    })
+    .catch(err => res.status(404).json(err));
+})
+
+// GET api/profile/all
+// Get all profiles
+router.get('/all', (req, res) => {
+    const errors = {};
+    Profile.find()
+    .populate('user', ['name', 'avatar'])
+    .then(profiles => {
+        if(!profiles) {
+            errors.noProfile = 'There are no profiles';
+            return res.status(404).json({errors});
+        }
+    })
+    .catch(err => res.status(404).json({profile: 'There are no profiles'}));
+})
 
 // POST api/profile
 // Create or edit user profile
