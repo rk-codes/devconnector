d46272818a2e2f7eb79405cd8d990c1c6b5de9e8
rk-codes <ping.you.back@gmail.com>
2018-08-16 16:59:43 -0700

    add links

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
 
 export default class Navbar extends Component {
   render() {
@@ -29,14 +30,14 @@ export default class Navbar extends Component {
 
             <ul className="navbar-nav ml-auto">
               <li className="nav-item">
-                <a className="nav-link" href="register.html">
+                <Link className="nav-link" to="/register">
                   Sign Up
-                </a>
+                </Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="login.html">
+                <Link className="nav-link" to="/login">
                   Login
-                </a>
+                </Link>
               </li>
             </ul>
           </div>
